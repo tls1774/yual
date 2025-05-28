4432e9bf127e7b4c96fa85581e2f604fd341ceb6
tls1774 <tls1774@naver.com>
2025-05-28 18:01:08 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -153,4 +153,26 @@ $(document).ready(function(){
     })
 
     /**************************** find 탭 기능 : 끝 ********************* */
+
+    /************************ 분양 swiper ************************** */
+    const adopt_swiper = new Swiper('.adopt .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 640px 이상일때 적용 */
+                spaceBetween: 24,
+                centeredSlides: true
+            },
+        },
+        centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+      
+        navigation: {
+            nextEl: '.adopt .list_ctrl .btn_next',
+            prevEl: '.adopt .list_ctrl .btn_prev',
+        },
+    });
+
+
+    /************************ 분양 swiper 끝 ************************** */
 })
