38cdf0afc324ee993bc9cab42066031225e7e92f
tls1774 <tls1774@naver.com>
2025-06-04 18:00:55 +0900

    .

--- kumho/js/common.js
+++ kumho/js/common.js
@@ -18,6 +18,7 @@ let scrolling //브라우저가 스크롤 된 값
 let scroll_prev //이전에 스크롤 된 값
 let window_w //브라우저의 넓이 값
 let mobile_size = 1024 // 모바일로 변경되는 사이트
+let menu_open //모바일에서 사용할 메뉴가 열렸는지 여부
 
 $(window).scroll(function(){ //브라우저가 스크롤 될 때마다 1번 실행 
     // console.log ('브라우저가 스크롤 된다 된다 된다된장!!!!!!!!!! ')
@@ -51,6 +52,29 @@ $(document).ready(function(){//문서가 로딩 되고 단 1번 실행
     })
     $(' header .gnb .gnb_close').on('click' , function(){
         $('header').removeClass('menu_open')
+    })
+
+     /*************** 모바일 2차 메뉴 열고닫기  ******************* 
+      * 지금 현재 메뉴가 열려있는지 닫혀있는지 구분 (li에 open 클래스 있는지 유무)
+      * 메뉴가 열려있으면 - li에 open 클래스를 삭제, 2차 메뉴 접기 
+      * 메뉴가 닫혀있으면 - li에 open 클래스를 추가, 2차 메뉴 열기 
+     */
+    $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+        if( device_status == 'mobile'){
+            // console.log('눌려??????????????????????')
+            e.preventDefault()
+            menu_open = $(this).parents('li').hasClass('open') 
+            //console.log(menu_open)
+            if(menu_open == true){ //메뉴가 열려있으면
+                $(this).parents('li').removeClass('open')
+                $(this).next().slideUp()
+            }else{ //닫혀있으면
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('open')
+                $('header .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp()
+                $(this).parents('li').addClass('open')
+                $(this).next().slideDown()
+            }
+        }    
     })
 
 })
--- kumho/js/main.js
+++ kumho/js/main.js
@@ -30,6 +30,37 @@ $(document).ready(function(){
         $(this).hide()
         $('.visual .ctrl_wrap button.btn-stop').show()
     })
-     /*      ###########            Visual swiper 연결 : 끝      ##########          */
+    /*      ###########       Visual swiper 연결 : 끝      ##########     */
 
+    /*      ###########      news swiper 연결 : 시작      ##########     */
+
+    const news_swiper = new Swiper('.swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    /*      ###########      news swiper 연결 : 끝       ##########     */
+
+    /*      ###########      biz의 list에 마우스 오버 이벤트 : 시작    ##########    */
+    $('.biz .list ul li').on('mouseenter' , function(){
+        $('.biz .list ul li').removeClass('on')
+        $(this).addClass('on')
+    })
+
+    /*      ###########      biz의 list에 마우스 오버 이벤트 : 끝    ##########    */
 })
