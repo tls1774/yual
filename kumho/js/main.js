d4d665d8e05f3cbca0bc6a2db4f360771f360327
tls1774 <tls1774@naver.com>
2025-06-19 13:21:43 +0900

    .

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
     /**************** visual swiper 연결 : 끝 *****************/
 
     /**************** news swiper 연결 : 시작 *****************/
-    const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    const news_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
         spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
         breakpoints: {
@@ -47,25 +47,8 @@ $(document).ready(function(){
             },
         },
         //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        //loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        // autoplay: {  /* 팝업 자동 실행 */
-        //     delay: 2500,
-        //     disableOnInteraction: true,
-        // },
-        navigation: {
-            nextEl: '.news .ctrl_wrap .btn_next',
-            prevEl: '.news .ctrl_wrap .btn_prev',
-        },
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.news .ctrl_wrap .count', /* 해당 요소의 class명 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-        },
-        scrollbar: {
-            el: ".news .ctrl_wrap .swiper-scrollbar",
-            hide: false,
-            draggable: true,
-            //dragSize: 200,
-        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+      
     });
     /**************** news swiper 연결 : 끝 *****************/
 
