e1b85dcc8ac3e5bdb5e6f20eab60155e0d47f639
tls1774 <tls1774@naver.com>
2025-06-27 18:02:33 +0900

    .

--- somnium/js/main.js
+++ somnium/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+         
+           
             /*************************** header 와 메뉴 : 시작 *************************
         * pc인지 모바일인지 구분 - 브라우저 넓이로.. 
         * 스크롤값 계산 
@@ -14,11 +15,14 @@ $(document).ready(function(){
         //     delay: 5000,
         //     disableOnInteraction: true,
         // },
+
         effect: "fade", /* fade 효과 */
+
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
         el: '.visual .paging', /* 해당 요소의 class명 */
+        clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
         // type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
         // renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
         //     return '<span class="' + className + '">' + (index + 1) + "</span>";
@@ -26,27 +30,108 @@ $(document).ready(function(){
     },
 
         navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual .btn_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.visual .btn_wrap button.btn_prev',  
+            nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_prev',  
         },
+
         });
-        $('.visual .btn_wrap button.btn_stop').on('click', function(){
-            //console.log('일시정지 버튼 클릭')
-            visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-            $(this).hide() //정지버튼 자신은 숨김
-            $('.visual .btn_wrap button.btn_play').show() //재생 나타남
+      
+        /********************** header fixed  *********************/   
+
+        scroll_chk() //문서가 로딩되었을때 1번 실행
+        $(window).scroll(function(){ //스크롤 할때마다 1번씩 실행
+            scroll_chk() //함수의 실행
+        })//$(window).scroll
+
+        device_chk() // 문서가 로딩되었을때 1번 실행
+        $(window).resize(function(){ //브라우저가 리사이즈 할때마다 1번씩 실행
+            device_chk() //함수의 실행
+        })//$(window).resize
+
+        function scroll_chk(){
+            scrolling = $(window).scrollTop()
+            //console.log(scrolling)
+            if(scrolling > 0){
+                $('header').addClass('fixed')
+            }else{
+                $('header').removeClass('fixed')
+            }
+        }//function scroll_chk
+
+        function device_chk(){
+            win_w = $(window).width()
+            
+            if(win_w > 1024){ //1025 이상
+                device_status = 'pc'
+            }else{
+                device_status = 'mobile'
+            }
+            //console.log(device_status)
+        }
+
+
+        /*
+            pc버전에서만 
+            메뉴에 마우스를 오버하면 
+            >> header .gnb .gnb_wrap ul.depth1 > li 
+            1. header에 menu_over 클래스 추가
+            2. 오버한 1차메뉴 li에 over 클래스 추가
+            header .gnb .gnb_wrap ul.depth1 > li
+
+            언제 메뉴의 오버상태를 해제할꺼냐....
+            header 밖에 나가면 메뉴 사라지게..
+        */
+
+        
+        $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter focusin', function(){
+            //마우스를 오버했을때만 실행 (pc일때만 실행)
+            if(device_status == 'pc'){
+                $('header').addClass('menu_over')
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+                $(this).addClass('over')
+            }//if
         })
-        $('.visual .btn_wrap button.btn_play').on('click', function(){
-            //console.log('재생 버튼 클릭')
-            visual_swiper.autoplay.start();  /* 재생 기능 */
-            $(this).hide() //재생버튼 자신은 숨김
-            $('.visual .btn_wrap button.btn_stop').show() //정지버튼이 나타남
+        $('header').on('mouseleave', function(){
+            if(device_status == 'pc'){
+                $('header').removeClass('menu_over')
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+            }
         })
 
-        
+        $('header .gnb .gnb_wrap ul.depth1 > li:last-child > ul.depth2 > li:last-child > a').on('focusout', function(){
+            if(device_status == 'pc'){
+                $('header').removeClass('menu_over')
+                $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
+            }
+        })
         
 
-
-
-        
 })
+        //함수의 선언 
+        function resize_chk(){
+            window_w = $(window).width()
+            //console.log(window_w)
+            if(window_w > mobile_size){ //1024보다 크면
+                device_status = 'pc'
+            }else{ //같거나 작으면
+                device_status = 'mobile'
+            }
+            //console.log(device_status)
+        }
+        function scroll_chk(){
+            scroll_prev = scrolling //스크롤값을 다시 계산하기 전에 이전값을 prev에 저장
+            scrolling = $(window).scrollTop()
+            //console.log(scroll_prev, scrolling)
+            if(scrolling > 0){ //조금이라도 스크롤 됐으면
+                $('header').addClass('fixed')
+                if(scrolling > scroll_prev){
+                    //console.log('내려가는 중!!!!!!!!!!!!!!')
+                    $('header').addClass('gnb_up')
+                }else{
+                    //console.log('올라가는 중!!!!!!!!!!!!!!')
+                    $('header').removeClass('gnb_up')
+                }
+            }else{ //0일때
+                $('header').removeClass('fixed')
+            }
+        }
