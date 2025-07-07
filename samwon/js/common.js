019b81b444b0f672388021f94291e06b8ea562ba
tls1774 <tls1774@naver.com>
2025-07-08 01:56:42 +0900

    .

--- samwon/js/common.js
+++ samwon/js/common.js
@@ -2,34 +2,34 @@ let device_status //pc인지 모바일 구분하는 값
 let scrolling //브라우저가 스크롤 된 값
 let scroll_prev //이전에 스크롤 된값
 let window_w //브라우저의 넓이 값
-let mobile_size = 1024 //모바일로 변경되는 사이트
+let mobile_size = 1024 //모바일로 변경되는 사이즈
 let menu_open //모바일에서 사용할 메뉴가 열렸는지 여부
 
 $(window).scroll(function(){//브라우저가 스크롤 될때마다 1번 실행
-    //console.log('브라우저가 스크롤 된다 된다!!!!!!!!')
+    //console.log('브라우저가 스크롤 된다!!')
     scroll_chk()
 })
 $(window).resize(function(){//리사이즈 될때마다 1번실행
-    //console.log('브라우저 크기 변한다!!!!!!!!!!')
+    //console.log('브라우저 크기 변한다!!!')
     resize_chk() //함수의 실행
 })
 $(document).ready(function(){//문서가 로딩 되고 단 1번 실행
 
      /* TOP 버튼을 클릭하면 상단으로 스크롤 */
      $('footer .top').on('click' , function(){
-        // console.log('클릭됨 탑탑!!')
+        // console.log('탑 클릭됐음!!')
         $('html, body').animate({
             scrollTop: 0
         }, 500)
      })   
 
-    //console.log('로딩됐다!!!!!!!!!!!!!!!!')
+    //console.log('로딩됐다!!')
     resize_chk() //함수의 실행
     scroll_chk()
 
     $('header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function(){
         if(device_status == 'pc'){
-            //console.log('오버했따!!!!!!!!!!!!!!!')
+            //console.log('오버했다!!')
             $('header').addClass('menu_over')
             $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
             $(this).addClass('over')
@@ -39,7 +39,6 @@ $(document).ready(function(){//문서가 로딩 되고 단 1번 실행
         $('header').removeClass('menu_over')
         $('header .gnb .gnb_wrap ul.depth1 > li').removeClass('over')
     })
-
 
     /************ 모바일 메뉴 열고 닫기 ********** */
     $('header .gnb .gnb_open').on('click', function(){
@@ -72,33 +71,34 @@ $(document).ready(function(){//문서가 로딩 되고 단 1번 실행
         } 
     })
 
+    
 })
 
-//함수의 선언 
-function resize_chk(){
-    window_w = $(window).width()
-    //console.log(window_w)
-    if(window_w > mobile_size){ //1024보다 크면
-        device_status = 'pc'
-    }else{ //같거나 작으면
-        device_status = 'mobile'
-    }
-    //console.log(device_status)
-}
-function scroll_chk(){
-    scroll_prev = scrolling //스크롤값을 다시 계산하기 전에 이전값을 prev에 저장
-    scrolling = $(window).scrollTop()
-    //console.log(scroll_prev, scrolling)
-    if(scrolling > 0){ //조금이라도 스크롤 됐으면
-        $('header').addClass('fixed')
-        if(scrolling > scroll_prev){
-            //console.log('내려가는 중!!!!!!!!!!!!!!')
-            $('header').addClass('gnb_up')
-        }else{
-            //console.log('올라가는 중!!!!!!!!!!!!!!')
-            $('header').removeClass('gnb_up')
+    //함수의 선언 
+    function resize_chk(){
+        window_w = $(window).width()
+        //console.log(window_w)
+        if(window_w > mobile_size){ //1024보다 크면
+            device_status = 'pc'
+        }else{ //같거나 작으면
+            device_status = 'mobile'
         }
-    }else{ //0일때
-        $('header').removeClass('fixed')
+        //console.log(device_status)
     }
-}
+    function scroll_chk(){
+        scroll_prev = scrolling //스크롤값을 다시 계산하기 전에 이전값을 prev에 저장
+        scrolling = $(window).scrollTop()
+        //console.log(scroll_prev, scrolling)
+        if(scrolling > 0){ //조금이라도 스크롤 됐으면
+            $('header').addClass('fixed')
+            if(scrolling > scroll_prev){
+                //console.log('내려가는 중!!!!!!!!!!!!!!')
+                $('header').addClass('gnb_up')
+            }else{
+                //console.log('올라가는 중!!!!!!!!!!!!!!')
+                $('header').removeClass('gnb_up')
+            }
+        }else{ //0일때
+            $('header').removeClass('fixed')
+        }
+    }
--- samwon/js/main.js
+++ samwon/js/main.js
@@ -1,39 +1,84 @@
 $(document).ready(function(){
 
-     AOS.init({
-        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
-        duration: 500, // 애니메이션 효과가 작동되는 시간
-        easing: 'ease', // 가속도
+    AOS.init({
+        offset: 150,
+        duration: 500,
+        easing: 'ease',
     });
-    
-    /**************** visual swiper 연결 : 시작 *****************/
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 4000,
-            disableOnInteraction: true,
+
+    let progressFillTimer = null;
+
+    const visual_swiper = new Swiper('.visual .swiper', {
+        autoplay: {  
+        delay: 5000,
+        disableOnInteraction: true,
         },
-        effect: "fade", /* fade 효과 */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual .ctrl_wrap button.btn_next',  /* 다음 버튼의 클래스명 */
-            prevEl: '.visual .ctrl_wrap button.btn_prev',  
+        effect: "fade",
+        loop: true,
+        navigation: {  
+        nextEl: '.visual .ctrl_wrap button.btn_next',
+        prevEl: '.visual .ctrl_wrap button.btn_prev',
         },
+        on: {
+        init: function () {
+            updateSlideInfo(this);
+            animateProgressBar();
+            const activeSlide = this.slides[this.activeIndex];
+            const txt_Wrap = activeSlide.querySelector('.txt_wrap');
+            if (txt_Wrap) txt_Wrap.classList.add('on');
+        },
+        slideChange: function () {
+            updateSlideInfo(this);
+            animateProgressBar();
+        },
+        slideChangeTransitionStart: function () {
+            this.slides.forEach(slide => {
+            const txt_Wrap = slide.querySelector('.txt_wrap');
+            if (txt_Wrap) txt_Wrap.classList.remove('on');
+            });
+        },
+        slideChangeTransitionEnd: function () {
+            const activeSlide = this.slides[this.activeIndex];
+            const txt_Wrap = activeSlide.querySelector('.txt_wrap');
+            if (txt_Wrap) {
+            void txt_Wrap.offsetWidth;
+            txt_Wrap.classList.add('on');
+            }
+        }
+        }
     });
-    // visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-    // visual_swiper.autoplay.start();  /* 재생 기능 */
 
-    $('.visual .ctrl_wrap button.btn_stop').on('click', function(){
-        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
-        //console.log('정지버튼 클릭!!!!!!!!')
-        $(this).hide()
-        $('.visual .ctrl_wrap button.btn_play').show()
-    })
-    $('.visual .ctrl_wrap button.btn_play').on('click', function(){
-        //console.log('재생버튼')
-        visual_swiper.autoplay.start();  /* 재생 기능 */
-        $(this).hide()
-        $('.visual .ctrl_wrap button.btn_stop').show()
-    })
+    function updateSlideInfo(swiper) {
+        const totalSlides = swiper.slides.length - (swiper.loopedSlides || 0) * 2;
+        const currentSlide = swiper.realIndex + 1;
+        document.querySelector('.visual .current-slide').textContent = currentSlide.toString().padStart(2, '0');
+        document.querySelector('.visual .total-slide').textContent = totalSlides.toString().padStart(2, '0');
+    }
 
-    /**************** visual swiper 연결 : 끝 *****************/
-})
+    function animateProgressBar() {
+        const progressFill = document.querySelector('.visual .progress-fill');
+
+        // 기존 애니메이션 정지
+        if (progressFillTimer) {
+        clearTimeout(progressFillTimer);
+        progressFillTimer = null;
+        }
+
+        // 1) 즉시 리셋
+        progressFill.style.transition = 'none';
+        progressFill.style.width = '0%';
+
+        // 2) 다음 프레임에 애니메이션 실행
+        requestAnimationFrame(() => {
+        progressFill.style.transition = 'width 4s linear'; // Swiper autoplay 시간과 맞춤
+        progressFill.style.width = '100%';
+        });
+
+        // 3) 다음 슬라이드에서 다시 리셋할 수 있도록 타이머 저장
+        progressFillTimer = setTimeout(() => {
+        progressFill.style.transition = 'none';
+        progressFill.style.width = '0%';
+        }, 5000); // 4초 후에 다시 초기화
+    }
+
+});
