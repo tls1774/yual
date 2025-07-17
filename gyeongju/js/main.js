8ee2169d63432e9fc40a99d762633407f23f42ab
tls1774 <tls1774@naver.com>
2025-07-17 18:03:39 +0900

    .

--- gyeongju/js/main.js
+++ gyeongju/js/main.js
@@ -1,42 +1,52 @@
-let device_status //pc인지 모바일 구분하는 값
-let scrolling //브라우저가 스크롤 된 값
-let scroll_prev //이전에 스크롤 된값
-let window_w //브라우저의 넓이 값
-let mobile_size = 1024 //모바일로 변경되는 사이즈
-let menu_open //모바일에서 사용할 메뉴가 열렸는지 여부
-
 $(document).ready(function(){
-    // console.log("알랄라")
 
+    const culture_swiper = new Swiper('.culture .content .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+    spaceBetween: 24, /* 팝업과 팝업 사이 여백 */
+    breakpoints: {
+        768: {    /* 640px 이상일때 적용 */
+            slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            spaceBetween: 24,
+        },
+    },
+    centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+    loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    // autoplay: {  /* 팝업 자동 실행 */
+    //     delay: 2500,
+    //     disableOnInteraction: true,
+    // },
+    
+    });
+    
+    const write_swiper = new Swiper('.write .txt .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
-    /************ 모바일 메뉴 열고 닫기 ********** */
-    $('header .header_body .gnb .gnb_open').on('click', function(){
-        $('.header_body').addClass('menu_open')
-    })
-    $('header .header_body .gnb .gnb_close').on('click', function(){
-        $('.header_body').removeClass('menu_open')
-    })
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 4000,
+            disableOnInteraction: true,
+        },
 
-    /************ 모바일 2차 메뉴 열고 닫기 ********** 
-     * 지금현재 메뉴가 열려있는지 닫혀있는지 구분 (li에 open클래스 있는지 유무)
-     * 메뉴가 열려있으면 - li에 open클래스를 삭제, 2차 메뉴 접기
-     * 메뉴가 닫혀있으면 - li에 open클래스를 추가, 2차 메뉴 열기
-    */
-    $('header .header_body .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
-        if(device_status == 'mobile'){
-            //console.log('눌린다 눌린다!!!!!!!!!!!!!')
-            e.preventDefault()
-            menu_open = $(this).parents('li').hasClass('open') 
-            //console.log(menu_open)
-            if(menu_open == true){ //메뉴가 열려있으면
-                $(this).parents('li').removeClass('open')
-                $(this).next().slideUp()
-            }else{ //닫혀있으면
-                $('header .header_body .gnb .gnb_wrap ul.depth1 > li').removeClass('open')
-                $('header .header_body .gnb .gnb_wrap ul.depth1 > li > ul.depth2').slideUp()
-                $(this).parents('li').addClass('open')
-                $(this).next().slideDown()
-            }
-        } 
-    })
+        effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
+        
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.btn_prev',  
+        },
+
+    });
+    
+
+    
+    
 })
